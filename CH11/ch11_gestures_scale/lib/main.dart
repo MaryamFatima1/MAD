2312ce553fc296e1f1f1ac8ac868d0415b8eb575
Maryam Fatima <maryamfatiii12@gmail.com>
2023-12-11 21:03:10 +0500

    chapter 11
    chapter 11

---
+++ CH11/ch11_gestures_scale/lib/main.dart
@@ -0,0 +1,20 @@
+
+import 'package:ch11_gestures_scale/pages/dismissible.dart';
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyApps(),
+    );
+  }
+}
